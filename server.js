ddce102dc89c1b4021f0e04139144819d00eb718
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-11-15 19:56:13 -0700

    git init

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+app.use(express.json())
+app.use(cors());
+
+const port = 5000
+app.set('port', process.env.PORT || 5000);
+app.locals.title = 'Trackr API';
+
+app.locals.savedRepos = [];
+
+app.get('/api/v1/repos', (req, res) => {
+  const repos = app.locals.savedRepos;
+  res.send(repos)
+})
+
+app.post('/api/v1/repos', (req, res) => {
+  const newRepo = req.body;
+  const isRepoSaved = app.locals.savedRepos.some(repo => repo.id === newRepo.id);
+
+  if (isRepoSaved) {
+    res.status(400).send("Repo already in list")
+  }
+  else {
+    app.locals.savedRepos.push(newRepo);
+    res.send(app.locals.savedRepos);
+}})
+
+app.delete('/api/v1/repos/:id', (request, response) => {
+	const foundRepo = app.locals.savedRepos.find((repo) => repo.id === parseInt(request.params.id));
+
+	if (!foundRepo) {
+		return response.status(404).json('Repo not found');
+	}
+
+	app.locals.savedRepos = app.locals.savedRepos.filter((repo) => repo.id !== parseInt(request.params.id));
+
+	response.status(200).send(app.locals.savedRepos);
+});
+
+app.listen(port, () => {
+  console.log(`Trackr API is listening at http://localhost:${port}`)
+})
